e555c26586ee4561d5efc1daf04c42e9ea57c00c
mgmilton <matt.milton@gmail.com>
2018-03-28 10:10:59 -0600

    adds params to search

--- spec/features/user_can_locate_nearest_fuel_spec.rb
+++ spec/features/user_can_locate_nearest_fuel_spec.rb
@@ -6,12 +6,12 @@ feature "User sees a list of nearest fuel stations" do
       VCR.use_cassette("user_sees_fuel", :record => :new_episodes) do
         visit "/"
 
-        fill_in "q", with: 80203
+        fill_in "zip", with: 80203
 
         click_on "Locate"
         expect(current_path).to eq("/search")
         expect(current_url).to include("zip=80203")
-        expect(current_url).to include("fuel_type=ELEC,LPG")
+        expect(current_url).to include("fuel_type=ELEC%2CLPG")
         expect(current_url).to include("distance=6")
         expect(page).to have_content("10 stations")
         expect(page).to have_css(".station", count: 10)
